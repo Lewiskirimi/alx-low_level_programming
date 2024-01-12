628fc191e94f31602400bd1a8383648420693a1d
LewisKirimi <lewisstudies254@gmail.com>
2024-01-12 00:29:23 -0800

    hello and printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * program to print text below
+ *
+ * Return is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
