6f3675e486ef2ebed2857065247980ced0a4eb7b
LewisKirimi <lewisstudies254@gmail.com>
2024-01-12 12:56:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program to print alphabets
+ *
+ * Return: value 0
+*/
+int main(void) 
+{
+	char x = 97;
+	while(x<=122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return 0;
+}
