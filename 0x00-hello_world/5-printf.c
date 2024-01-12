4551fa3454ddb03f85e6889c68e16e2fe9e1a822
LewisKirimi <lewisstudies254@gmail.com>
2024-01-12 00:40:24 -0800

    helloo and printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - entry point
  * program to print text below
  *
- * Return is 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
