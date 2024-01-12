f5f0c6d31fa81f9784dba172a70b40b641bc2ed8
LewisKirimi <lewisstudies254@gmail.com>
2024-01-12 14:35:15 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * program to print lowercase alphabet without e and q
+ *
+ * Return: value is 0
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while(x <= 'z')
+	{
+		if((x != 'q') && (x != 'e'))
+		{
+		putchar(x);
+		}
+		x++;
+	}
+	return (0);
+}
