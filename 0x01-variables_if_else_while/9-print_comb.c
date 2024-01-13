7b1fdd3853d91b9b8c831d2e2fd95c9299a5ad2b
LewisKirimi <lewisstudies254@gmail.com>
2024-01-12 16:25:59 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int x = 0;
-	char a = 36, y = 44, z = 32;
 
 	while (x < 10)
 	{
 		putchar('0' + x);
 		if (x < 9)
 		{
-			putchar(y);
-			putchar(z);
+			putchar(',');
+			putchar(' ');
 		}
 		x++;
 	}
