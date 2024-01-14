2040ea27a7439a688091fb58379ff8ef8b639c33
LewisKirimi <lewisstudies254@gmail.com>
2024-01-14 13:15:12 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program to print 3 digits separately
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = x + 1; y < 9; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				putchar((z % 10) + '0');
+				if ((x == 7) && (y == 8) && (z == 10))
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
