2c4c04a0f71920e85f7430c6feeb9bd87e299229
LewisKirimi <lewisstudies254@gmail.com>
2024-01-16 12:20:40 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN.H
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,5 +11,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
+void print_to_98(int n)
+	;
 #endif
