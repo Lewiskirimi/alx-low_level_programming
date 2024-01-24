341fabd0f040e8f455d4a5ada050842f67fa86c9
LewisKirimi <lewisstudies254@gmail.com>
2024-01-24 14:47:46 -0800

    upcasing strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\n'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] > 96 && s[i] < 123)
 		{
 			s[i] -= 32;
 		}
 	}
-	return (0);
+	return (s);
 }
