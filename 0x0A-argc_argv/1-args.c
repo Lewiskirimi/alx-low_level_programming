d9c79f169ed425f63db177c07e05fa8fae24b32e
LewisKirimi <lewisstudies254@gmail.com>
2024-01-31 17:58:44 -0800

    args0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: counts
+ * @argv: value
+ *
+ * Return: null
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
